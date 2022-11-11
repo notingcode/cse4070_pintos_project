682bc4ef90b1ec58815c59f9b7f939d8fedc12b8
notingcode <notingcode@gmail.com>
2022-11-12 01:07:37 +0900

    fixed timer.c

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -89,11 +89,10 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
-  int64_t start = timer_ticks ();
-
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  if (ticks < 0)
+    return;
+  thread_sleep(ticks); // put the thread which will be blocked soon into the blocked list
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
@@ -165,13 +164,17 @@ timer_print_stats (void)
 {
   printf ("Timer: %"PRId64" ticks\n", timer_ticks ());
 }
-
+
 /* Timer interrupt handler. */
 static void
 timer_interrupt (struct intr_frame *args UNUSED)
 {
+  if (thread_mlfqs)
+    thread_exit();
+
   ticks++;
-  thread_tick ();
+  thread_wakeup();
+  thread_tick();
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
